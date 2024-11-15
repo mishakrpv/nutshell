1dda536920593ff2d44d4ce723cb31e602cabb86
mishakrpv <mishavkrpv@gmail.com>
2024-11-15 17:25:12 +0300

    fix config dir location

--- src/config.rs
+++ src/config.rs
@@ -7,15 +7,13 @@ pub fn config_path() -> Result<PathBuf> {
     let dir = match env::var_os("_NUTSH_CONFIG_DIR") {
         Some(path) => PathBuf::from(path),
         None => dirs::config_local_dir()
-            .context("could not find config directory, please set _NUTSH_CONFIG_DIR manually")?,
+            .context("could not find config directory, please set _NUTSH_CONFIG_DIR manually")?
+            .join("nutshell"),
     };
 
     let path = dir.join("nutshell.lua");
 
-    ensure!(
-        path.is_absolute(),
-        "_NUTSH_CONFIG_DIR must be an absolute path"
-    );
+    ensure!(path.is_absolute(), "_NUTSH_CONFIG_DIR must be an absolute path");
     Ok(path)
 }
 
@@ -25,11 +23,9 @@ pub fn echo() -> bool {
 
 pub fn max_latency_ms() -> Result<u16> {
     let latency: u16 = match env::var_os("_NUTSH_MAX_LATENCY_MS") {
-        Some(latency) => latency
-            .into_string()
-            .unwrap_or("5000".to_string())
-            .parse::<u16>()
-            .unwrap(),
+        Some(latency) => {
+            latency.into_string().unwrap_or("5000".to_string()).parse::<u16>().unwrap()
+        }
         None => 5000_u16,
     };
 
